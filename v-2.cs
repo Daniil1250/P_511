c296fc35ee05b78350b57b4b22d417e79b58d8df
Daniil1250 <145423259+Daniil1250@users.noreply.github.com>
2026-04-08 15:30:08 +0300

    Update v-2.cs

--- v-2.cs
+++ v-2.cs
@@ -1,18 +1,46 @@
+using System.Linq.Expressions;
 using Microsoft.Data.SqlClient;
 
-string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=InternetShop;Integrated Security=True;";
+
+string masterConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=master;Integrated Security=True;";
+
+string dbName = "Magaz";
+
 
 try
 {
+    using (SqlConnection conn = new SqlConnection(masterConnectionString))
+    {
+        conn.Open();
+        Console.WriteLine("Подключение к master выполнено!");
+
+        string createDbSql = $"CREATE DATABASE [{dbName}]";
+        SqlCommand cmd = new SqlCommand(createDbSql, conn);
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine($"База данных '{dbName}' создана");
+
+        string checkSql = "SELECT DB_NAME() AS CurrentDB";
+        SqlCommand checkCmd = new SqlCommand(checkSql, conn);
+
+        conn.ChangeDatabase(dbName);
+        string currentDb = checkCmd.ExecuteScalar().ToString();
+        Console.WriteLine($"Текущая БД: {currentDb}");
+
+        conn.Close();
+    }
+
+
+
+string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Magaz;Integrated Security=True;";
+
     using (SqlConnection conn = new SqlConnection(ConnectionString))
     {
         conn.Open();
-        Console.WriteLine("Подключение к InternetShop открыто!");
+        Console.WriteLine("Подключение к Magaz выполнено!");
 
-    
         string createTableSql = @"
-            IF OBJECT_ID('Products', 'U') IS NULL
-            CREATE TABLE Products(
+                CREATE TABLE Products (
                 Id INT PRIMARY KEY,
                 Name VARCHAR(100),
                 Price DECIMAL(10,2)
@@ -20,25 +48,20 @@ try
 
         SqlCommand createCmd = new SqlCommand(createTableSql, conn);
         createCmd.ExecuteNonQuery();
-        Console.WriteLine("Таблица Products создана (или уже существует)!");
+        Console.WriteLine("Таблица Products создана!");
 
-    
         string insertSql = @"
-            IF NOT EXISTS (SELECT * FROM Products WHERE Id = 1)
-            INSERT INTO Products VALUES 
-            (1, 'Tivi', 8999.0),
-            (2, 'ПК', 1599.0),
-            (3, 'Телефон', 1969.0)";
+                INSERT INTO Products VALUES
+                (1, 'Pk', 89990.00),
+                (2, 'MacBook', 159000.00),
+                (3, 'Play', 8000.00),
+                (4, 'iPhone', 90000.00);
+            ";
 
         SqlCommand insertCmd = new SqlCommand(insertSql, conn);
         int rows = insertCmd.ExecuteNonQuery();
+        Console.WriteLine($"Добавлено {rows}");
 
-        if (rows > 0)
-            Console.WriteLine($"Добавлено {rows} строк");
-        else
-            Console.WriteLine("Данные уже существуют");
-
-    
         string readSql = "SELECT * FROM Products";
         SqlCommand readCmd = new SqlCommand(readSql, conn);
 
@@ -47,20 +70,13 @@ try
             Console.WriteLine("\n=== Товары в базе ===");
             while (reader.Read())
             {
-                string name = reader.GetString(1);
-                Console.WriteLine($"ID: {reader["Id"]}, Name: {name}, Price: {reader["Price"]} руб.");
+                Console.WriteLine($"ID: {reader["Id"]}, Name: {reader["Name"]}, Price: {reader["Price"]} руб.");
             }
         }
 
         conn.Close();
-    }
-}
-catch (SqlException ex)
-{
+    } }
+catch (SqlException ex) {
     Console.WriteLine($"Ошибка SQL: {ex.Message}");
     Console.WriteLine($"Номер ошибки: {ex.Number}");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Общая ошибка: {ex.Message}");
 }
